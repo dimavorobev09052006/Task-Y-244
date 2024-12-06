d5a7e9e6aa2da3d36c581aac6ba9e525ae8e8e6c
dimavorobev09052006 <celolote0905@gmail.com>
2024-12-06 12:44:41 +0300

    пр11.py

--- пр11/пр11.py
+++ пр11/пр11.py
@@ -1,43 +1,25 @@
-import tkinter as tk
-from tkinter import messagebox
-import requests # type: ignore
+import requests
 import json
+from pprint import pprint
 
-def fetch_repo_info():
-    repo_name = entry.get().strip()
-    if not repo_name:
-        messagebox.showerror("Ошибка", "Введите имя репозитория в формате 'owner/repo'")
-        return
+username = "Ansible"
+url = f"https://api.github.com/users/{username}"
+# делаем запрос и возвращаем json
+user_data1 = requests.get(url).json()
 
-    url = f"https://api.github.com/repos/{repo_name}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        repo_data = response.json()
-        result = {
-            'company': None,
-            'created_at': repo_data.get('created_at'),
-            'email': None,
-            'id': repo_data.get('id'),
-            'name': repo_data.get('name'),
-            'url': f"https://api.github.com/users/{repo_data.get('owner', {}).get('login')}"
+print(user_data1)
+print('-'*25)
+s1 = json.dumps(user_data1)
+user_data = json.loads(s1)
+info = {
+        'company'   : (user_data["company"]   ),
+        'created_at': (user_data["created_at"]),
+        'email'     : (user_data["email"]     ),
+        'id'        : (user_data["id"]        ),
+        'name'      : (user_data["name"]      ),
+        'url'       : (user_data["url"]       )
         }
+pprint(info)
 
-        with open(f"{repo_data.get('name')}_info.json", 'w') as json_file:
-            json.dump(result, json_file, indent=4)
-
-        messagebox.showinfo("Успех", f"Информация о репозитории сохранена в файл {repo_data.get('name')}_info.json")
-    else:
-        messagebox.showerror("Ошибка", f"Не удалось получить данные о репозитории: {response.status_code} ({response.text})")
-
-root = tk.Tk()
-root.title("GitHub Repo Info Fetcher")
-
-tk.Label(root, text="Введите имя репозитория (в формате 'owner/repo'):").pack(pady=10)
-
-entry = tk.Entry(root, width=50)
-entry.pack(pady=10)
-
-tk.Button(root, text="Получить информацию", command=fetch_repo_info).pack(pady=20)
-
-root.mainloop()
+with open('Json.txt', 'w') as file_json:
+  json.dump(info,file_json)
